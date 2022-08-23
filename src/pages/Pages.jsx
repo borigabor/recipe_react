e0cad1ccfd02594e61544eecffbe804f3c286800
unknown <borigabor08@gmail.com>
2022-08-23 12:11:55 +0200

    add file

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from '../pages/Home';
+import Searched from './Searched';
+import {Route, Routes} from 'react-router-dom';
+import Cuisine from './Cuisine';
+import Recipe from './Recipe';
+
+
+function Pages() {
+
+
+
+  return (
+    <Routes>
+    <Route  path='/' element={<Home />} />
+    <Route path='/cuisine/:type' element={<Cuisine />} />
+    <Route path='/searched/:search' element={<Searched />} />
+    <Route path='/recipe/:name' element={<Recipe />} />
+    </Routes>
+  );
+}
+
+export default Pages;
